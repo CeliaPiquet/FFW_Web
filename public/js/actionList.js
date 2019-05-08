963697eacc80f3a2197030fbfcb1a6401d00a652
CeliaPiquet <celiapiquet@gmail.com>
2019-05-08 18:19:12 +0200

    reload on create

--- public/js/actionList.js
+++ public/js/actionList.js
@@ -65,7 +65,7 @@ function addProduct(){
         var jsonProduct = JSON.stringify(newProduct);
         var request = new XMLHttpRequest();
         request.onreadystatechange = function(){
-            if(request.readyState === 4 && request.status === 200){
+            if(request.readyState === 4 && request.status === 201){
                 document.location.reload(true);
             } 
         }
